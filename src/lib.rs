1d2f04e75160cdb85577090fd35d66600c0c23f8
Kevin Navero <Kevin.Navero@nextest.com>
2021-08-02 18:14:03 -0700

    inject failing test

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,13 @@ mod tests {
     use super::add;
 
     #[test]
-    fn it_works() {
+    fn add_two_and_one() {
         assert_eq!(add(2, 1), 3);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(add(2, 2), 4);
     }
 }
 
